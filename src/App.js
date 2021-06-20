edb8f0d3d73cf5681139a706bae121604340a832
Aleksey Sokolov <lionsbet@outlook.com>
2021-06-21 00:45:58 +0300

    5 lesson 35minutes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import { Home , Cart } from "./pages";
 
 
 function App() {
+
+    const [pizzas, setPizzas] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('http://localhost:3000/db.json').then((resp) => resp.json()).then(json => {
+            setPizzas(json.pizzas)
+        })
+    }, []);
+    console.log('pizzas!', pizzas)
   return (
       <div className="wrapper">
           <Header />
